b471d6b99b7f585537a407f6153662ff151c3e6c
Infinita <infinita740@gmail.com>
2015-11-01 18:56:34 +0100

    correction zones d'affichage
    Les zones d'affichages remarchent et l'animation n'est plus dupliquée.
    Suppression des fichiers js inutiles et non utilisés.

--- js/saisieaffichage.js
+++
@@ -1,8 +0,0 @@
-console.log("== début affichage ==");
-var canvas = document.getElementById("saisieaffichage");
-var MAX_WIDTH=550;
-var MAX_HEIGHT=490;
-canvas.width=MAX_WIDTH;
-canvas.height=MAX_HEIGHT;
-console.log(canvas.height);
-var ctx = canvas.getContext("2d");
