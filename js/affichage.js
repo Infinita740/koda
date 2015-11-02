b854b8bc60ca4f2198dbaa834cfd5aa70628f095
Infinita <infinita740@gmail.com>
2015-11-02 17:07:39 +0100

    fonction drawLevel qui utilise les fichiers dans /levels
    réécriture de la fonction drawLevel pour prendre un chiffre en
    paramètre.
    Les niveaux sont dessinés en utilisant les valeurs précisées dans le
    fichier levelX.js

--- js/affichage.js
+++ js/affichage.js
@@ -7,6 +7,7 @@ canvas.height=MAX_HEIGHT;
 console.log(canvas.height);
 var ctx = canvas.getContext("2d");
 
+
 var now,
     then = new Date().getTime(),
     delta;
@@ -34,7 +35,7 @@ function moveRight(){
     // console.log(delta);
     
     ctx.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
-    drawLevel1();
+    drawLevel(1);
     var c = circle;
     c.draw();
     c.x += calcSpeed(delta, 5);
@@ -56,125 +57,56 @@ var animloop = function(){
     requestAnimationFrame(animloop);
     moveRight();
 }
- 
- var level = JSON.parse(level1);
-function drawLevel1(){
+
+
+function drawLevel(lvl){
     
+    //choix du niveau ici
+    if (lvl==1) {var level = level1;};
+
 	//fond
     ctx.beginPath();
     ctx.moveTo(0,0);
-    ctx.lineTo(0,500);
-    ctx.lineTo(500,500);
-    ctx.lineTo(500,0);
+    ctx.lineTo(0,MAX_WIDTH);
+    ctx.lineTo(MAX_HEIGHT,MAX_WIDTH);
+    ctx.lineTo(MAX_HEIGHT,0);
     ctx.lineWidth = 1;
     ctx.fillStyle = "gray";
     ctx.fill();
 
-    //chemin
-	
-    ctx.beginPath();
-    ctx.moveTo(level.moveTo[0],level.moveTo[1]);
-    ctx.lineTo(level.lineTo1[0],level.lineTo1[1]);
-    ctx.lineTo(level.lineTo2[0],level.lineTo2[1]);
-    ctx.lineTo(level.lineTo3[0],level.lineTo3[1]);
-    ctx.fillStyle = level.color;
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(25,25);
-    ctx.lineTo(475,25);
-    ctx.lineTo(475,50);
-    ctx.lineTo(25,50);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(450,25);
-    ctx.lineTo(475,25);
-    ctx.lineTo(475,475);
-    ctx.lineTo(450,475);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(250,475);
-    ctx.lineTo(250,450);
-    ctx.lineTo(450,450);
-    ctx.lineTo(450,475);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(250,250);
-    ctx.lineTo(275,250);
-    ctx.lineTo(275,475);
-    ctx.lineTo(250,475);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(250,250);
-    ctx.lineTo(375,250);
-    ctx.lineTo(375,275);
-    ctx.lineTo(250,275);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(375,250);
-    ctx.lineTo(375,150);
-    ctx.lineTo(350,150);
-    ctx.lineTo(350,250);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(375,125);
-    ctx.lineTo(125,125);
-    ctx.lineTo(125,150);
-    ctx.lineTo(375,150);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(125,125);
-    ctx.lineTo(150,125);
-    ctx.lineTo(150,350);
-    ctx.lineTo(125,350);
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(100,350);
-    ctx.lineTo(100,400);
-    ctx.lineTo(175,400);
-    ctx.lineTo(175,350);
-    ctx.fillStyle = "red";
-    ctx.fill();
+    //chemin, début et fin
+    for (var i=0; i < level.length; i++) {
+        ctx.beginPath();
+        var item = level[i];
+        ctx.moveTo(item.moveTo[0],item.moveTo[1]);
+        ctx.lineTo(item.lineTo1[0],item.lineTo1[1]);
+        ctx.lineTo(item.lineTo2[0],item.lineTo2[1]);
+        ctx.lineTo(item.lineTo3[0],item.lineTo3[1]);
+        ctx.fillStyle = item.color;
+        ctx.fill();
+    };
 	
 	//quadrillage
 	var i=25;
-	while(i<500){
+	while(i<MAX_WIDTH){
 		ctx.beginPath();
 		ctx.moveTo(i,0);
-		ctx.lineTo(i,500);
+		ctx.lineTo(i,MAX_HEIGHT);
 		ctx.lineWidth=0.75;
 		ctx.strokeStyle="black";
 		ctx.stroke();
 		i=i+25;
 	}
 	var g = 25;
-	while(g<500){
+	while(g<MAX_HEIGHT){
 		ctx.beginPath();
 		ctx.moveTo(0,g);
-		ctx.lineTo(500,g);
+		ctx.lineTo(MAX_WIDTH,g);
 		ctx.lineWidth=0.75;
 		ctx.strokeStyle="black";
 		ctx.stroke();
 		g=g+25;
 	}
 }
-
-
 
 animloop();
---
+++ levels/level1.js
@@ -0,0 +1,81 @@
+var level1=[
+    {   
+        "color" : "green",
+        "moveTo":[0,0],
+        "lineTo1":[25,0],
+        "lineTo2":[25,75],
+        "lineTo3":[0,75]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[25,25],
+        "lineTo1":[475,25],
+        "lineTo2":[475,50],
+        "lineTo3":[25,50]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[450,25],
+        "lineTo1":[475,25],
+        "lineTo2":[475,475],
+        "lineTo3":[450,475]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[250,475],
+        "lineTo1":[250,450],
+        "lineTo2":[450,450],
+        "lineTo3":[450,475]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[250,250],
+        "lineTo1":[275,250],
+        "lineTo2":[275,475],
+        "lineTo3":[250,475]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[250,250],
+        "lineTo1":[375,250],
+        "lineTo2":[375,275],
+        "lineTo3":[250,275]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[375,250],
+        "lineTo1":[375,150],
+        "lineTo2":[350,150],
+        "lineTo3":[350,250]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[375,125],
+        "lineTo1":[125,125],
+        "lineTo2":[125,150],
+        "lineTo3":[375,150]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[125,125],
+        "lineTo1":[150,125],
+        "lineTo2":[150,350],
+        "lineTo3":[125,350]
+    },
+
+    {
+        "color" : "red",
+        "moveTo":[100,350],
+        "lineTo1":[100,400],
+        "lineTo2":[175,400],
+        "lineTo3":[175,350]
+    }
+];
