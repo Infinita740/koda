0459272f0868ad463d1e7f0e0fdc23f7dfd77f98
Infinita <infinita740@gmail.com>
2015-11-12 17:58:34 +0100

    drag & drop + clonage

--- js/drag.js
+++ js/drag.js
@@ -1,13 +1,42 @@
+var elem = {"avancer" : 1,
+            "reculer" : 20,
+            "tantque" : 60,
+            "tourner" : 40}
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+function drag_clone(ev) {
+    ev.dataTransfer.setData("id_drag", elem[ev.target.id]);
+    
+    var item = document.getElementById(ev.target.id);
+    clone = item.cloneNode(true);
+    clone.id=elem[ev.target.id];
+    
+    clone.ondragstart = drag;
+    //clone.addEventListener("dragstart", drag, false);
+
+    elem[ev.target.id]+=1;
+
+    console.log(clone);
+    document.getElementById("invisible").appendChild(clone);
+}
+
+function drag(ev){
+    ev.dataTransfer.setData("id_drag", ev.target.id);
 }
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var data = ev.dataTransfer.getData("id_drag");
+    /*if (ev.target.id == 'saisie' || ev.target.id == 'tantque'){
+        ev.target.appendChild(document.getElementById(data));
+    }*/
+    console.log(data);
+    data = String(data);
+    if (ev.target.id > 59 || ev.target.id == "saisie") {
+        ev.target.appendChild(document.getElementById(data));
+    };
+    
 }
