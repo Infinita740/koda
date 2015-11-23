30c6ca0e11c65f2910bd136c642779d8eab2bb6a
JoffreyBoutaud <joffreyboutaud@gmail.com>
2015-11-23 16:41:34 +0100

    Liste deroulante

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 console.log("== début du programme ==");
+
+function generer_select_avancer(nb) {
+	console.log(".");
+	var quelquechose = document.createElement('SELECT');
+	for (var i = 1; i <= 20; i++) {
+		var caseVoulut = document.createElement('OPTION');
+		caseVoulut.value = "" + i;
+		caseVoulut.innerHTML = "" + i;
+		quelquechose.appendChild(caseVoulut);
+		console.log(caseVoulut);
+	};
+	var test = document.getElementById("avancer");
+	test.appendChild(quelquechose);
+}
+
+function generer_select_reculer(nb) {
+	console.log(".");
+	var quelquechose = document.createElement('SELECT');
+	for (var i = 1; i <= 20; i++) {
+		var caseVoulut = document.createElement('OPTION');
+		caseVoulut.value = "" + i;
+		caseVoulut.innerHTML = "" + i;
+		quelquechose.appendChild(caseVoulut);
+		console.log(caseVoulut);
+	};
+	var test = document.getElementById("reculer");
+	test.appendChild(quelquechose);
+}
