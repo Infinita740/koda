c201b9a21f54021d87de33dbd88df709074d7c6d
JoffreyBoutaud <joffreyboutaud@gmail.com>
2015-11-26 09:43:21 +0100

    level 2

--- levels/level2.js
+++ levels/level2.js
@@ -10,7 +10,51 @@ var level2=[
     {
         "color" : "white",
         "moveTo":[25,25],
-        "lineTo1":[475,25],
-        "lineTo2":[475,50],
+        "lineTo1":[50,25],
+        "lineTo2":[50,50],
         "lineTo3":[25,50]
     },
+
+];
+
+p1 = 50;
+p2 = 25;
+p3 = 75;
+p4 = 50;
+
+for (var i = 0; i < 16; i++) {
+	level2.push(
+		{
+			"color" : "white",
+    	    "moveTo":[p1,p2],
+    	    "lineTo1":[p3,p2],
+    	    "lineTo2":[p3,p3],
+    	    "lineTo3":[p4,p3]
+    
+    	}
+	);
+	p1 = p1 + 25;
+	p2 = p2 + 25;
+	p3 = p3 + 25;
+	p4 = p4 + 25;
+};
+
+level2.push(
+	{
+        "color" : "white",
+        "moveTo":[450,425],
+        "lineTo1":[475,425],
+        "lineTo2":[475,450],
+        "lineTo3":[450,450]
+    }
+);
+
+level2.push(
+	{
+        "color" : "red",
+        "moveTo":[450,450],
+        "lineTo1":[475,450],
+        "lineTo2":[475,475],
+        "lineTo3":[450,475]
+    }
+);
