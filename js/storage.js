74cf90c943cc58dd829a5da8dee3df116f3fde49
Infinita <infinita740@gmail.com>
2015-11-30 16:31:42 +0100

    bouton get_level qui lit le localStorage

--- js/storage.js
+++ js/storage.js
@@ -1,10 +1,24 @@
-function levelCounter(){
+function save_level(){
     if(typeof(Storage) !== "undefined") 
     {
         localStorage['chosen_level'] = chosen_level;
         alert("Mémorisation effectuée");
     } 
 
+    else 
+    {
+        alert("localStorage n'est pas supporté");
+    }
+}
+
+function get_level(){
+    if(typeof(Storage) !== undefined) 
+    {
+        if(localStorage['chosen_level']!= undefined)
+        {
+            alert(localStorage['chosen_level']);
+        }
+    }
     else 
     {
         alert("localStorage n'est pas supporté");
