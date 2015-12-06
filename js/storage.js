dbeb4598ad8962a6efea92f2ceb42099f1de6f78
MrFondue <corentin.hauray8@etu.univ-lorraine.fr>
2015-12-07 00:02:25 +0100

    Bouton Réinitialiser
    Supprime le local storage

--- js/storage.js
+++ js/storage.js
@@ -31,3 +31,22 @@ function get_level(){
         alert("localStorage n'est pas supporté");
     }
 }
+
+function reset_level(){
+    if(typeof(Storage) !== undefined) 
+    {
+        localStorage['chosen_level'] = chosen_level;
+        var nodes = $("#saisie").html();
+        console.log(nodes);
+        var nodes_json = JSON.stringify(nodes);
+        console.log(nodes_json);
+        localStorage.removeItem('saisie', nodes_json);
+        //alert("Mémorisation effectuée");
+    } 
+
+    else 
+    {
+        alert("localStorage n'est pas supporté");
+    }
+
+}
