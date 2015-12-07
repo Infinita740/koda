cca76f47577ce7fa4e23c428ccfdf9866080299e
MrFondue <corentin.hauray8@etu.univ-lorraine.fr>
2015-12-07 17:16:20 +0100

    Local storage
    fonction permettant d'enregistrer un level terminé et son score dans un
    tableau

--- js/storage.js
+++ js/storage.js
@@ -50,3 +50,45 @@ function reset_level(){
     }
 
 }
+
+var resultat = [];
+resultat[1] = 0;
+resultat[2] = 0;
+
+
+function enregistrement_score(lvl, score){
+    if(typeof(Storage) !== undefined) 
+    {
+        /*if(chosen_level == win){
+             localStorage['level_termine','score'] = chosen_level, score[1];
+        var nodes = $("#level_termine", "#score").html();
+        console.log(nodes);
+        var nodes_json = JSON.stringify(nodes);
+        console.log(nodes_json);
+        localStorage.setItem('level_termine','score', nodes_json);*/
+        //alert("Mémorisation effectuée");
+        resultat[lvl] = score;
+
+        var resultat_JSON = JSON.stringify(resultat);
+        localStorage['resultat'] = resultat_JSON;
+       
+    } 
+    else 
+    {
+        alert("localStorage n'est pas supporté");
+    }
+
+}
+
+function chargement_score(){
+    if(typeof(Storage) !== undefined) 
+    {
+        resultat = JSON.parse(localStorage['resultat']);
+       
+    } 
+    else 
+    {
+        alert("localStorage n'est pas supporté");
+    }
+
+}
