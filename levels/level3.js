10c58ec0d120256180a424cd16a78e27b039cd46
JoffreyBoutaud <joffreyboutaud@gmail.com>
2016-01-05 15:49:56 +0100

    Level 3

--- levels/level3.js
+++ levels/level3.js
@@ -21,7 +21,6 @@ p1 = 50;
 p2 = 25;
 p3 = 75;
 p4 = 50;
-
 for (var i = 0; i < 16; i++) {
 	level3.push(
 		{
@@ -30,7 +29,6 @@ for (var i = 0; i < 16; i++) {
     	    "lineTo1":[p3,p2],
     	    "lineTo2":[p3,p3],
     	    "lineTo3":[p4,p3]
-    
     	}
 	);
 	p1 = p1 + 25;
@@ -42,20 +40,41 @@ for (var i = 0; i < 16; i++) {
 level3.push(
 	{
         "color" : "white",
-        "moveTo":[450,425],
-        "lineTo1":[475,425],
-        "lineTo2":[475,450],
-        "lineTo3":[450,450]
+        "moveTo":[400,450],
+        "lineTo1":[450,450],
+        "lineTo2":[450,475],
+        "lineTo3":[400,475]
     }
 );
 
+p1 = 50;
+p2 = 100;
+p3 = 75;
+p4 = 150;
+for (var i = 0; i < 14; i++) {
+    level3.push(
+        {
+            "color" : "white",
+            "moveTo":[p1,p2],
+            "lineTo1":[p3,p2],
+            "lineTo2":[p3,p4],
+            "lineTo3":[p1,p4]
+    
+        }
+    );
+    p1 = p1 + 25;
+    p2 = p2 + 25;
+    p3 = p3 + 25;
+    p4 = p4 + 25;
+};
+
 level3.push(
-	{
+    {
         "color" : "red",
-        "moveTo":[450,450],
-        "lineTo1":[475,450],
-        "lineTo2":[475,475],
-        "lineTo3":[450,475]
+        "moveTo":[25,100],
+        "lineTo1":[50,100],
+        "lineTo2":[50,125],
+        "lineTo3":[25,125]
     }
 );
 
