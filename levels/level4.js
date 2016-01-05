fa66aa9101b77cdca82626a2b842da3278e73899
JoffreyBoutaud <joffreyboutaud@gmail.com>
2016-01-05 17:29:36 +0100

    Level 4 et 5

---
+++ levels/level4.js
@@ -0,0 +1,52 @@
+var level4=[
+    {   
+        "color" : "green",
+        "moveTo":[0,0],
+        "lineTo1":[25,0],
+        "lineTo2":[25,75],
+        "lineTo3":[0,75]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[25,25],
+        "lineTo1":[275,25],
+        "lineTo2":[275,50],
+        "lineTo3":[25,50]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[250,50],
+        "lineTo1":[275,50],
+        "lineTo2":[275,175],
+        "lineTo3":[250,175]
+    },
+
+    {
+        "color" : "orange",
+        "moveTo":[225,175],
+        "lineTo1":[300,175],
+        "lineTo2":[300,300],
+        "lineTo3":[225,300]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[250,300],
+        "lineTo1":[275,300],
+        "lineTo2":[275,425],
+        "lineTo3":[250,425]
+    },
+
+    {
+        "color" : "red",
+        "moveTo":[225,425],
+        "lineTo1":[300,425],
+        "lineTo2":[300,475],
+        "lineTo3":[225,475]
+    },
+
+];
+
+var indication4 = "Premier changement de couleur";
---
+++ levels/level5.js
@@ -0,0 +1,92 @@
+var level5=[
+    {   
+        "color" : "green",
+        "moveTo":[0,0],
+        "lineTo1":[25,0],
+        "lineTo2":[25,75],
+        "lineTo3":[0,75]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[25,25],
+        "lineTo1":[175,25],
+        "lineTo2":[175,50],
+        "lineTo3":[25,50]
+    },
+
+    {
+        "color" : "purple",
+        "moveTo":[175,0],
+        "lineTo1":[325,0],
+        "lineTo2":[325,75],
+        "lineTo3":[175,75]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[325,25],
+        "lineTo1":[475,25],
+        "lineTo2":[475,50],
+        "lineTo3":[325,50]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[450,50],
+        "lineTo1":[475,50],
+        "lineTo2":[475,175],
+        "lineTo3":[450,175]
+    },
+
+    {
+        "color" : "blue",
+        "moveTo":[425,175],
+        "lineTo1":[500,175],
+        "lineTo2":[500,325],
+        "lineTo3":[425,325]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[450,325],
+        "lineTo1":[475,325],
+        "lineTo2":[475,475],
+        "lineTo3":[450,475]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[325,450],
+        "lineTo1":[475,450],
+        "lineTo2":[450,475],
+        "lineTo3":[325,475]
+    },
+
+    {
+        "color" : "yellow",
+        "moveTo":[175,425],
+        "lineTo1":[325,425],
+        "lineTo2":[325,500],
+        "lineTo3":[175,500]
+    },
+
+    {
+        "color" : "white",
+        "moveTo":[25,450],
+        "lineTo1":[175,450],
+        "lineTo2":[175,475],
+        "lineTo3":[25,475]
+    },
+
+    {
+        "color" : "red",
+        "moveTo":[0,425],
+        "lineTo1":[25,425],
+        "lineTo2":[25,500],
+        "lineTo3":[0,500]
+    },
+
+];
+
+var indication5 = "Multiple changement de couleur";
