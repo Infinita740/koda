282fa878ebaf199bb31e7a9bf9b51601646f6e48
Infinita <infinita740@gmail.com>
2016-01-12 00:08:06 +0100

    fonction pour voir le code ok

--- js/voir_le_code.js
+++ js/voir_le_code.js
@@ -1,4 +1,4 @@
-function voir_le_code()
+function show_code()
 {
     var tab_code = [];
     var liste = document.getElementById("saisie").childNodes;
@@ -31,9 +31,45 @@ function voir_le_code()
                         }
                     };
                 };
+                console.log("-> ", nom_action, num_action);
+                tab_code.push([nom_action, num_action]);
             }
-            console.log(nom_action, num_action);
-            tab_code.push([num_action, nom_action]);
+            else{
+                //cas du tant que :
+                var select = liste[i].childNodes[1];
+                //récupération de la valeur du tq :
+                for(var j = 0; j < select.childElementCount; j++){
+                    if(select[j].selected){
+                        num_action = select[j].value;
+                        tab_code.push([nom_action, num_action])
+                    }
+                }
+
+                //récupération de l'intérieur du tq
+                actions = liste[i].childNodes[2];
+                for (var k = 0; k<actions.childNodes.length; k++){
+                    nom_action = actions.childNodes[k].actionPerso;
+                    var opt = actions.childNodes[k].childNodes[1];
+                    for (var j = 0; j < opt.length; j++) 
+                {
+                    if (opt[j].selected) 
+                    {
+                        //récupération de la valeur sélectionnée
+                        if (nom_action != "couleur") 
+                        {
+                            num_action = parseInt(opt[j].value);
+                        }
+                        else
+                        {
+                            num_action = opt[j].value;
+                        }
+                    };
+                };
+                tab_code.push([nom_action, num_action]);
+                }
+                tab_code.push(['ftq', '/']);
+            }
+            
         }
 
     }
@@ -45,4 +81,38 @@ function voir_le_code()
             {
                 alert("localStorage n'est pas supporté");
             }
+}
+
+function gen_code(){
+    console.log("test");
+    var codes = JSON.parse(localStorage['tab_code']);
+    console.log(codes);
+    var func = {
+        "droite":"deplacerDroite",
+        "gauche":"deplacerGauche",
+        "haut":"deplacerHaut",
+        "bas":"deplacerBas",
+        "couleur":"changerCouleur"
+    }
+    var indent = false;
+    var code_affichage = $("#code_js").html();
+    for(var j = 0; j < codes.length; j++){
+
+        if(codes[j][0] != "tantque" && codes[j][0] != "ftq"){
+            if(indent){code_affichage+="    "};
+            
+            if(codes[j][0] == "couleur") {code_affichage += func[codes[j][0]] + "(\"" + codes[j][1] + "\");\n"; }
+            else {code_affichage += func[codes[j][0]] + "(" + codes[j][1] + ");\n";}
+        }
+        if(codes[j][0] == "ftq"){
+            indent = false;
+            code_affichage += "}\n";
+        }
+        if(codes[j][0] == "tantque"){
+            indent = true;
+            code_affichage += "\nfor(var i = 0; i < " + codes[j][1] + "; i ++){\n";
+        }
+        
+    }
+    $("#code_js").html(code_affichage);
 }
